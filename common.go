665a0238750edb55963dc0630f4b0da4fb37fc8d
wolfwml <wolf_wml@163.com>
2019-02-27 16:40:59 +0800

    support tls

--- common.go
+++ common.go
@@ -2,54 +2,76 @@ package ngrokbridge
 
 import (
 	"bufio"
+	"crypto/tls"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"sync"
 	"time"
 )
 
-func StartTcpServer(addr string, serveHandler func(net.Conn)) error {
+type TcpHandler interface {
+	Handle(net.Conn)
+}
+
+type TcpFuncHandler func(net.Conn)
+
+func (self TcpFuncHandler) Handle(conn net.Conn) {
+	self(conn)
+}
+
+func StartTcpServer(addr string, serveHandler TcpHandler) error {
 
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Println(err)
 		return err
 	}
 
 	for {
 		conn, _ := l.Accept()
 		if conn != nil {
-			go serveHandler(conn)
+			go serveHandler.Handle(conn)
 		}
 	}
 }
 
-func Start(addr string) {
-	StartTcpServer(addr, func(conn net.Conn) {
-		defer conn.Close()
-		//Wrap the conn with TeeReader
-		c, r := NewConn(conn)
-		c.SetReadDeadline(time.Now().Add(time.Second * 2))
-		request, err := http.ReadRequest(bufio.NewReader(r))
-		if err != nil {
-			return
-		}
-		request.Body.Close()
+func handler(conn net.Conn) {
+	defer conn.Close()
+	//Wrap the conn with TeeReader
+	c, r := NewConn(conn)
+	c.SetReadDeadline(time.Now().Add(time.Second * 2))
+	request, err := http.ReadRequest(bufio.NewReader(r))
+	if err != nil {
+		return
+	}
+	request.Body.Close()
 
-		tunnel, err := GetTunnel(request.URL.Path)
-		if err != nil {
-			return
-		}
-		cServer, err := tunnel.GetProxy()
-		if err != nil {
-			return
-		}
+	tunnel, err := GetTunnel(request.URL.Path)
+	if err != nil {
+		return
+	}
+	cServer, err := tunnel.GetProxy()
+	if err != nil {
+		return
+	}
 
-		c.SetDeadline(time.Time{})
-		Join(cServer, c)
-	})
+	c.SetDeadline(time.Time{})
+	Join(cServer, c)
+}
+
+func Start(addr string) error {
+	return StartTcpServer(addr, TcpFuncHandler(handler))
+}
+
+func StartTLS(addr string) error {
+	return StartTcpServer(addr, TcpFuncHandler(func(conn net.Conn) {
+		config := &tls.Config{}
+		config.Certificates = make([]tls.Certificate, 1)
+		config.Certificates[0] = rootCert
+		//Set the connection as TLS connection
+		tlsConn := tls.Server(conn, config)
+		handler(tlsConn)
+	}))
 }
 
 //Copy data between two conn in full duplex mode,
